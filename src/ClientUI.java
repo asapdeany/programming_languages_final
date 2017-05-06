0b7df90a3e85f92a6458f021400d9c38d9058013
asapdeany <dspon15@stlawu.edu>
2017-05-06 16:43:31 -0400

    Initial commit

---
+++ src/ClientUI.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by deansponholz on 5/6/17.
+ */
+public class ClientUI extends JPanel implements ActionListener {
+
+    public ClientUI(JFrame parent){
+
+        JPanel allpanels = new JPanel();
+        allpanels.setLayout(new BorderLayout());
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+        JFrame jFrame = new JFrame("Final Project");
+        //jFrame.add()
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+}
